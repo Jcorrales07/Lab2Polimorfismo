738d72b97728ca04bf72a50ec8170e18be7e3e6c
Joe Corrales <corralesjoe07@gmail.com>
2022-02-11 18:50:45 -0600

    me quede sin tiempo ;C

--- src/joecorralespolimorfismo/Lab4P2_JoeCorrales_22141156.java
+++ src/joecorralespolimorfismo/Lab4P2_JoeCorrales_22141156.java
@@ -182,12 +182,31 @@ public class Lab4P2_JoeCorrales_22141156 {
             Collections.shuffle(montesco.aldeanos);
             int i = 0;
             do {
-                
+                Aldeano pMontesco = montesco.aldeanos.get(i);
+                Aldeano pVS = familiaVs.aldeanos.get(i); 
+
+                while(pMontesco.getPtsVida() != 0 && pVS.getPtsVida() != 0) {
+                    double dolor = pMontesco.dolorGenerado(pVS);
+                    if (dolor != 0) {
+                        pVS.setPtsVida((int) (pVS.getPtsVida() - dolor));
+                        System.out.println(pMontesco.getNombre() + " ataco a " + pVS.getNombre() + " causandole "
+                                + dolor + " de daño! ");
+                        System.out.println("La vida de " + pVS.getNombre() + " es: " + pVS.getPtsVida() );
+                    }
+                    double dolorVS = pVS.dolorGenerado(pMontesco);
+                    if (dolorVS != 0) {
+                        pMontesco.setPtsVida((int) (pMontesco.getPtsVida() - dolorVS));
+                        System.out.println(pVS.getNombre() + " ataco a " + pMontesco.getNombre() + " causandole "
+                                + dolorVS + " de daño! ");
+                        System.out.println("La vida de " + pMontesco.getNombre() + " es: " + pVS.getPtsVida() );
+                    }
+                    if (pMontesco.getPtsVida() != 0 && pVS.getPtsVida() < 0) {
+                        System.out.println(pMontesco.getNombre() + " ha ganado!");
+                    }
+                }
+                i++;
             } while(!montesco.aldeanos.isEmpty() && !familiaVs.aldeanos.isEmpty());
-                
-            
         }
-        
     }
     
     
